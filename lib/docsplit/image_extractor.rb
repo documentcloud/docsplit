4f3daa06a6f58375c7ef2312e5863c6a78b9f48a
Jeremy Ashkenas <jashkenas@gmail.com>
2010-03-04 12:22:32 -0500

    Adding '+adjoin' to the gm command to enable it to work with GraphicsMagick 1.3.11

--- lib/docsplit/image_extractor.rb
+++ lib/docsplit/image_extractor.rb
@@ -22,7 +22,7 @@ module Docsplit
       directory = File.join(@output, subfolder)
       FileUtils.mkdir_p(directory) unless File.exists?(directory)
       out_file  = File.join(directory, "#{basename}_%05d.#{format}")
-      cmd = "gm convert #{DENSITY_ARG} #{resize_arg(size)} #{quality_arg(format)} \"#{pdf}#{pages_arg}\" \"#{out_file}\" 2>&1"
+      cmd = "gm convert +adjoin #{DENSITY_ARG} #{resize_arg(size)} #{quality_arg(format)} \"#{pdf}#{pages_arg}\" \"#{out_file}\" 2>&1"
       result = `#{cmd}`.chomp
       raise ExtractionFailed, result if $? != 0
       renumber_images(out_file, format)
