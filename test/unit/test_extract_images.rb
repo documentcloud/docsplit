69972a3b6cc90687c0efb8d031ad7709d4c9496b
Jeremy Ashkenas <jashkenas@gmail.com>
2010-06-16 07:32:41 -0400

    somehow broke my installation of libpng on this machine ... switching the image tests over to use gifs.

--- test/unit/test_extract_images.rb
+++ test/unit/test_extract_images.rb
@@ -3,13 +3,13 @@ require 'test_helper'
 class ExtractImagesTest < Test::Unit::TestCase
 
   def test_basic_image_extraction
-    Docsplit.extract_images('test/fixtures/obama_arts.pdf', :size => "250x", :output => OUTPUT)
-    assert Dir["#{OUTPUT}/*"] == ['test/output/obama_arts_1.png', 'test/output/obama_arts_2.png']
+    Docsplit.extract_images('test/fixtures/obama_arts.pdf', :format => :gif, :size => "250x", :output => OUTPUT)
+    assert Dir["#{OUTPUT}/*"] == ['test/output/obama_arts_1.gif', 'test/output/obama_arts_2.gif']
   end
 
   def test_image_formatting
-    Docsplit.extract_images('test/fixtures/obama_arts.pdf', :format => [:jpg, :png], :size => "250x", :output => OUTPUT)
-    assert Dir["#{OUTPUT}/*.png"].length == 2
+    Docsplit.extract_images('test/fixtures/obama_arts.pdf', :format => [:jpg, :gif], :size => "250x", :output => OUTPUT)
+    assert Dir["#{OUTPUT}/*.gif"].length == 2
     assert Dir["#{OUTPUT}/*.jpg"].length == 2
   end
 
@@ -19,13 +19,13 @@ class ExtractImagesTest < Test::Unit::TestCase
   end
 
   def test_image_sizes
-    Docsplit.extract_images('test/fixtures/obama_arts.pdf', :size => ["50x", "150x"], :output => OUTPUT)
-    assert File.size("#{OUTPUT}/50x/obama_arts_1.png") < File.size("#{OUTPUT}/150x/obama_arts_1.png")
+    Docsplit.extract_images('test/fixtures/obama_arts.pdf', :format => :gif, :size => ["50x", "150x"], :output => OUTPUT)
+    assert File.size("#{OUTPUT}/50x/obama_arts_1.gif") < File.size("#{OUTPUT}/150x/obama_arts_1.gif")
   end
 
   def test_encrypted_images
-    Docsplit.extract_images('test/fixtures/encrypted.pdf', :size => "50x", :output => OUTPUT)
-    assert File.size("#{OUTPUT}/encrypted_1.png") > 100
+    Docsplit.extract_images('test/fixtures/encrypted.pdf', :format => :gif, :size => "50x", :output => OUTPUT)
+    assert File.size("#{OUTPUT}/encrypted_1.gif") > 100
   end
 
   def test_password_protected_extraction
@@ -35,10 +35,10 @@ class ExtractImagesTest < Test::Unit::TestCase
   end
 
   def test_repeated_extraction_in_the_same_directory
-    Docsplit.extract_images('test/fixtures/obama_arts.pdf', :size => "250x", :output => OUTPUT)
-    assert Dir["#{OUTPUT}/*"] == ['test/output/obama_arts_1.png', 'test/output/obama_arts_2.png']
-    Docsplit.extract_images('test/fixtures/obama_arts.pdf', :size => "250x", :output => OUTPUT)
-    assert Dir["#{OUTPUT}/*"] == ['test/output/obama_arts_1.png', 'test/output/obama_arts_2.png']
+    Docsplit.extract_images('test/fixtures/obama_arts.pdf', :format => :gif, :size => "250x", :output => OUTPUT)
+    assert Dir["#{OUTPUT}/*"] == ['test/output/obama_arts_1.gif', 'test/output/obama_arts_2.gif']
+    Docsplit.extract_images('test/fixtures/obama_arts.pdf', :format => :gif, :size => "250x", :output => OUTPUT)
+    assert Dir["#{OUTPUT}/*"] == ['test/output/obama_arts_1.gif', 'test/output/obama_arts_2.gif']
   end
 
 end
