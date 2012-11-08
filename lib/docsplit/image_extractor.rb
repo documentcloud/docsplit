f10122123a4edfd6ef6dc05aab4bc247bedb4abe
Dermot Haughey <dermot.haughey@rvibe.com>
2012-11-08 14:42:48 -0600

    Cleaning up code

--- lib/docsplit/image_extractor.rb
+++ lib/docsplit/image_extractor.rb
@@ -22,11 +22,12 @@ module Docsplit
         end
       end
       case @return_value
+      when :images
+        images.reject{|i| i.nil? or i.empty?}
       when :intermediates
         @pdfs
       else
         @pdfs
-
       end
     end
 
@@ -43,6 +44,7 @@ module Docsplit
       escaped_pdf = ESCAPE[pdf]
       FileUtils.mkdir_p(directory) unless File.exists?(directory)
       common    = "#{MEMORY_ARGS} -density #{@density} #{resize_arg(size)} #{quality_arg(format)}"
+      image_paths = []
       if previous
         FileUtils.cp(Dir[directory_for(previous) + '/*'], directory)
         result = `MAGICK_TMPDIR=#{tempdir} OMP_NUM_THREADS=2 gm mogrify #{common} -unsharp 0x0.5+0.75 \"#{directory}/*.#{format}\" 2>&1`.chomp
@@ -50,11 +52,13 @@ module Docsplit
       else
         page_list(pages).each do |page|
           out_file  = ESCAPE[File.join(directory, "#{basename}_#{page}.#{format}")]
+          image_paths << out_file
           cmd = "MAGICK_TMPDIR=#{tempdir} OMP_NUM_THREADS=2 gm convert +adjoin -define pdf:use-cropbox=true #{common} #{escaped_pdf}[#{page - 1}] #{out_file} 2>&1".chomp
           result = `#{cmd}`.chomp
           raise ExtractionFailed, result if $? != 0
+
         end
-        return out_file
+        return image_paths
       end
     ensure
       FileUtils.remove_entry_secure tempdir if File.exists?(tempdir)
