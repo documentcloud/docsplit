08dbf33272f28c996b124af343563720543e02c5
Dermot Haughey <dermot.haughey@rvibe.com>
2012-11-13 13:48:13 -0600

    Refine specs

--- test/unit/test_extract_images.rb
+++ test/unit/test_extract_images.rb
@@ -12,30 +12,17 @@ class ExtractImagesTest < Test::Unit::TestCase
     assert Dir["#{OUTPUT}/*.gif"].length == 2
     assert Dir["#{OUTPUT}/*.jpg"].length == 2
   end
-  def test_image_extraction_specifying_return_value
+
+  def test_image_extraction_return_value
     def images_present?(formats, val)
       formats.map do |format|
         val.any?(Regexp.new(format))
       end.reduce(&:and)
     end
-    pdf_path = 'test/fixtures/obama_arts.pdf'
-    ret_value = Docsplit.extract_images(pdf_path, :format => [:jpg, :gif], :size => "250x", :output => OUTPUT, :and_return => [:images])
-    assert ret_value.length == 2
-
-    ret_value = Docsplit.extract_images(pdf_path, :format => [:jpg, :gif], :size => "250x 50x", :output => OUTPUT, :and_return => [:images])
-    assert ret_value.length == 4
-    assert images_present([:jpg, :gif], ret_value)
-   
-     intermediate_ret_value = Docsplit.extract_images(pdf_path, :format => [:jpg, :gif], :size => "250x 50x", :output => OUTPUT, :and_return => [:intermediate])
-    assert intermediate_ret_value.length == 1
-    assert !images_present?([:jpg, :gif], intermediate_ret_value)
-    assert intermediate.ret_value.any?(/\.pdf/)
-
-    non_specified_ret_value = Docsplit.extract_images(pdf_path, :format => [:jpg, :gif], :size => "250x 50x", :output => OUTPUT)
-    assert non_specified_ret_value.lenght == 1
-    assert !images_present?([:jpg, :gif], intermediate_ret_value)
-    assert non_specified_ret_value == intermediate_ret_value
-
+    return_value = Docsplit.extract_images('test/fixtures/obama_arts.pdf', :format => [:jpg, :gif], :size => "250x 50x", :output => OUTPUT)
+    assert return_value.lenght == 4
+    assert images_present?([:jpg, :gif], return_value)
+    assert return_value.is_a?(Enumerable)
   end
 
   def test_page_ranges
--- test/unit/test_extract_text.rb
+++ test/unit/test_extract_text.rb
@@ -4,9 +4,12 @@ require 'tmpdir'
 class ExtractTextTest < Test::Unit::TestCase
 
   def test_paged_extraction
-    Docsplit.extract_text('test/fixtures/obama_arts.pdf', :pages => 'all', :output => OUTPUT)
+    return_value = Docsplit.extract_text('test/fixtures/obama_arts.pdf', :pages => 'all', :output => OUTPUT)
     assert Dir["#{OUTPUT}/*.txt"].length == 2
     assert File.read("#{OUTPUT}/obama_arts_1.txt").match("Paid for by Obama for America")
+    assert return_value.is_a?(Enumerable)
+    assert return_value.all?(/\.txt/)
+    assert return_value.length == 2
   end
 
   def test_page_only_extraction
@@ -24,18 +27,24 @@ class ExtractTextTest < Test::Unit::TestCase
   end
 
   def test_unicode_extraction
-    Docsplit.extract_text('test/fixtures/unicode.pdf', :pages => 'all', :output => OUTPUT)
+    return_value = Docsplit.extract_text('test/fixtures/unicode.pdf', :pages => 'all', :output => OUTPUT)
     assert Dir["#{OUTPUT}/*.txt"].length == 3
+    assert return_value.is_a?(Enumerable)
+    assert return_value.all?(/\.txt/)
+    assert return_value.length == 3
   end
 
   def test_ocr_extraction
-    Docsplit.extract_text('test/fixtures/corrosion.pdf', :pages => 'all', :output => OUTPUT)
+    return_value =Docsplit.extract_text('test/fixtures/corrosion.pdf', :pages => 'all', :output => OUTPUT)
     4.times do |i|
       file = "corrosion_#{i + 1}.txt"
       assert_directory_contains(OUTPUT, file)
       assert File.read(File.join(OUTPUT, file)).size > 1, "Expected that file with extracted text should have reasonable size"
     end
+    assert return_value.is_a?(Enumerable)
+    assert return_value.all?(/\.txt/)
   end
+
 
   def test_ocr_extraction_in_mock_language
     exception = assert_raise(Docsplit::ExtractionFailed) {Docsplit.extract_text('test/fixtures/corrosion.pdf', :pages => 'all', :output => OUTPUT, :language => "mock")}
