fa6835e4e6e671bfb38582923a7ec8adb96c4990
ypxing <ypxing@gmail.com>
2014-10-14 12:41:09 +1100

    Windows:  "%d" is always escaped to "\%d".
    Unders Windows, "%d" is always escaped to "\%d". It makes "extract_pages" fail because of below exception.
    
    Docsplit::ExtractionFailed: Unhandled Java Exception in create_output():
    java.io.FileNotFoundException: ./abc_\1.pdf: The system cannot find the path specified.

--- lib/docsplit/page_extractor.rb
+++ lib/docsplit/page_extractor.rb
@@ -9,13 +9,13 @@ module Docsplit
       extract_options opts
       [pdfs].flatten.each do |pdf|
         pdf_name = File.basename(pdf, File.extname(pdf))
-        page_path = File.join(@output, "#{pdf_name}_%d.pdf")
+        page_path = ESCAPE[File.join(@output, "#{pdf_name}")] + "_%d.pdf"
         FileUtils.mkdir_p @output unless File.exists?(@output)
         
         cmd = if DEPENDENCIES[:pdftailor] # prefer pdftailor, but keep pdftk for backwards compatability
-          "pdftailor unstitch --output #{ESCAPE[page_path]} #{ESCAPE[pdf]} 2>&1"
+          "pdftailor unstitch --output #{page_path} #{ESCAPE[pdf]} 2>&1"
         else
-          "pdftk #{ESCAPE[pdf]} burst output #{ESCAPE[page_path]} 2>&1"
+          "pdftk #{ESCAPE[pdf]} burst output #{page_path} 2>&1"
         end
         result = `#{cmd}`.chomp
         FileUtils.rm('doc_data.txt') if File.exists?('doc_data.txt')
